0d455183e589d675902c9331df84f4f7f4fb700a
Astha Verma <41240026+astha24verma@users.noreply.github.com>
2023-09-05 23:41:09 +0530

    Update SetMatrixZeros.java

--- src/Day1/SetMatrixZeros.java
+++ src/Day1/SetMatrixZeros.java
@@ -2,6 +2,6 @@ package Day1;
 
 public class SetMatrixZeros {
     public static void main(String[] args) {
-        System.out.println( " here ");
+        System.out.println("Update from Github");
     }
 }
